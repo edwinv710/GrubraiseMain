02a599097c6900fdee9b4c4a323f2864d30ae055
edwin velasquez <ev9@buffalo.edu>
2012-11-15 00:15:06 -0500

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -46,11 +46,6 @@ ActiveRecord::Schema.define(:version => 20121112042640) do
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
-  create_table "pta", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "restuarants", :force => true do |t|
     t.string   "restuarant_name"
     t.string   "owner_name"
